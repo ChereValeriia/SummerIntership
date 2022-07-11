bebeddcf00ff11cd6fc0150bdaff4a8dc4c843d7
ChereValeriia <chere.valeriiia@gmail.com>
2022-07-11 15:24:53 +0400

    tried to do a service, but it was broken

--- src/main/java/com/company/internshipschedule/screen/lesson/LessonEdit.java
+++ src/main/java/com/company/internshipschedule/screen/lesson/LessonEdit.java
@@ -1,12 +1,38 @@
 package com.company.internshipschedule.screen.lesson;
 
-import com.company.internshipschedule.app.TeacherService;
+import io.jmix.core.DataManager;
+import io.jmix.core.FluentValuesLoader;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.Action;
 import io.jmix.ui.screen.*;
 import com.company.internshipschedule.entity.Lesson;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @UiController("Lesson.edit")
 @UiDescriptor("lesson-edit.xml")
 @EditedEntityContainer("lessonDc")
 public class LessonEdit extends StandardEditor<Lesson> {
+
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private DataManager dataManager;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Lesson> event) {
+        List<Lesson> data = dataManager.load(Lesson.class)
+                .query("select count(lesson.getTime()), count(lesson.getTeacher()) from Lessons where lesson.time = lesson.getTime() and lesson.teacher = lesson.getTeacher()")
+                .list();
+        if (data != null)
+        {
+            notifications.create()
+                    .withCaption("The same lesson is already existed")
+                    .withType(Notifications.NotificationType.WARNING)
+                    .show();
+        }
+        return;
+    }
 
 }
